4c930103a3b855130ec3bb8c1fc3e99745186ccd
DarkGenius <darkgenius@nextmail.ru>
2016-11-20 21:42:45 +0300

    task 2D

--- src/index.js
+++ src/index.js
@@ -122,6 +122,78 @@ let getPCJSON = async () => {
   return pc
 }
 
+app.get('/2D', (req, res) => {
+  let result = "Invalid color";
+  let color = req.query.color;
+  const re = [
+    /^#?([0-9a-f]{6})$/, // color = 123abc
+    /^#?([0-9a-f]{3})$/, // color = fff
+    /^rgb\(\s*([0-9]{1,3})\s*\,\s*([0-9]{1,3})\s*\,\s*([0-9]{1,3})\s*\)$/, // color = rgb()
+    /^hsl\(\s*([0-9]{1,3})\s*\,\s*([0-9]{1,3})\%\s*\,\s*([0-9]{1,3})\%\s*\)$/, // color = hsl()
+  ];
+
+  let hsl2rgb = (h,s,l) => {
+    console.log(`(h,s,l)=${h}, ${s}, ${l}`);
+    s = s / 100;
+    l = l / 100;
+    const c = (1 - Math.abs(2*l - 1))*s; //C = (1 - |2L - 1|) × S
+    const x = c * (1 - Math.abs((h/60) % 2 - 1)); //X = C × (1 - |(H / 60°) mod 2 - 1|)
+    const m = l - (c/2); // L - C/2
+    console.log(`(c,x,m)=${c}, ${x}, ${m}`);
+    let r1, g1, b1;
+    if (h >=0 && h < 60)
+      [r1, g1, b1] = [c, x, 0];
+    else if (h >=60 && h < 120)
+      [r1, g1, b1] = [x, c, 0];
+    else if (h >=120 && h < 180)
+      [r1, g1, b1] = [0, c, x];
+    else if (h >=180 && h < 240)
+      [r1, g1, b1] = [0, x, c];
+    else if (h >=240 && h < 300)
+      [r1, g1, b1] = [x, 0, c];
+    else if (h >=300 && h < 360)
+      [r1, g1, b1] = [c, 0, x];
+    console.log('rgb=', [r1, g1, b1].map(v => Math.round((v+m)*255)));
+    return [r1, g1, b1].map(v => Math.round((v+m)*255));
+  }
+
+  let trans = [
+    (t) =>`#${t[1]}`,
+    (t) => `#${t[1].split("").map((x) => x+x).join("")}`,
+    (t) => `#${[+t[1],+t[2],+t[3]].map((v) => {
+      const h = ~~(v/16);
+      const l = v % 16;
+      console.log(`v=${v} h=${h} l=${l}`)
+      return h.toString(16) + l.toString(16);
+    }).join("")}`,
+    (t) => trans[2]([null].concat(hsl2rgb(t[1], t[2], t[3])))
+  ];
+
+  let validators = [
+    null,
+    null,
+    (t) => [+t[1],+t[2],+t[3]].reduce((prev, curr) => prev && (curr>=0 && curr <=255), true),
+    (t) => [[+t[1], 0, 360],[+t[2], 0, 101],[+t[3], 0, 101]].reduce((prev, curr) => prev && (curr[0]>=curr[1] && curr[0] < curr[2]), true)
+  ]
+  console.log(`color=${color}`)
+  if (color){
+    color = color.toLowerCase().trim().replace(/%20/g, ' ')
+    let t;
+
+    for (let i=0, l = re.length; i<l; i++){
+      let t = color.match(re[i]);
+      if (t && (!validators[i] || validators[i](t))) {
+        result = trans[i](t);
+        break;
+      }
+    }
+
+
+  }
+
+  res.send(result);
+})
+
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
